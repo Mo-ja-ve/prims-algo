72c9267038059f37390391050dec4ab5f98d0ba7
0xBAMA <jon.baker1222@gmail.com>
2019-11-29 23:20:16 -0500

    getting weird seg fault

---
+++ gen.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using std::endl;
+using std::cout;
+#include <cstdlib>
+#include <cstring>
+#include <random>
+
+//call this with
+
+//    exe <low bound> <high bound>
+
+int main(int argc, char const *argv[])
+{
+  //random number generation
+  std::random_device rd;
+  std::mt19937 mt(rd());
+
+  if(!strcmp(argv[1], std::string("float").c_str()))
+  {
+    // cout << "floating point" << endl;
+    std::uniform_real_distribution<float> dist(atof(argv[2]), atof(argv[3]));
+    //output some samples
+    cout << dist(mt) <<  " " << dist(mt) << " " << dist(mt) << endl;
+  }
+  else if(!strcmp(argv[1], std::string("int").c_str()))
+  {
+    // cout << "integer" << endl;
+    std::uniform_int_distribution<int> dist(atoi(argv[2]), atoi(argv[3]));
+    //output some samples
+    cout << dist(mt) <<  " " << dist(mt) << " " << dist(mt) << endl;
+  }
+  else if(!strcmp(argv[1], std::string("list").c_str()))
+  {
+    std::uniform_int_distribution<int> dist1(atoi(argv[4]), atoi(argv[5])); //input values
+    std::uniform_int_distribution<int> dist2(0,atoi(argv[2])+1);  //nodes
+
+    cout << 1 << endl;
+
+    int num_nodes = atoi(argv[2]);
+    int num_links = atoi(argv[3]);
+
+    cout << num_nodes << " " << num_links  << endl; //number of nodes, number of links
+
+    for(int i = 0; i < num_links; i++)
+    {
+      cout << dist2(mt) << " " << dist2(mt) << " " << dist1(mt) << endl;  //first node, second node, weight
+    }
+  }
+
+  return 0;
+}
--- main.cpp
+++ main.cpp
@@ -17,8 +17,11 @@ typedef struct edge_t{
 void add_edge(vector<vector<edge>> &graph, int first, int second, int weight){
 
     edge temp;
+    
     temp.v = second;
     temp.weight = weight;
+
+    cout << "adding an edge between " << first << " and " << second << " with weight " << weight << endl;
 
     graph[first].push_back(temp);
 
@@ -46,7 +49,7 @@ void list_edges(vector<vector<edge>> graph){
             {
                 cout<< " no other nodes" << endl;
             }
-            
+
         }
     }else{
 
@@ -71,74 +74,75 @@ void load(vector<vector<edge > > &graph){
 
     for(int i = 0; i < case_links; i++)
     {
+        cout << " taking in number " << i << endl;
         cin >> first >> second >> weight;
 
         add_edge(graph, first, second, weight);
     }
 }
 
-// void primMST(vector<pair<int,int> > adj[], int V) 
-// { 
-//     // Create a priority queue to store vertices that 
-//     // are being preinMST. This is weird syntax in C++. 
-//     // Refer below link for details of this syntax 
-//     // http://geeksquiz.com/implement-min-heap-using-stl/ 
-//     priority_queue< iPair, vector <iPair> , greater<iPair> > pq; 
-  
-//     int src = 0; // Taking vertex 0 as source 
-  
-//     // Create a vector for keys and initialize all 
-//     // keys as infinite (INF) 
-//     vector<int> key(V, INF); 
-  
-//     // To store parent array which in turn store MST 
-//     vector<int> parent(V, -1); 
-  
-//     // To keep track of vertices included in MST 
-//     vector<bool> inMST(V, false); 
-  
-//     // Insert source itself in priority queue and initialize 
-//     // its key as 0. 
-//     pq.push(make_pair(0, src)); 
-//     key[src] = 0; 
-  
+// void primMST(vector<pair<int,int> > adj[], int V)
+// {
+//     // Create a priority queue to store vertices that
+//     // are being preinMST. This is weird syntax in C++.
+//     // Refer below link for details of this syntax
+//     // http://geeksquiz.com/implement-min-heap-using-stl/
+//     priority_queue< iPair, vector <iPair> , greater<iPair> > pq;
+
+//     int src = 0; // Taking vertex 0 as source
+
+//     // Create a vector for keys and initialize all
+//     // keys as infinite (INF)
+//     vector<int> key(V, INF);
+
+//     // To store parent array which in turn store MST
+//     vector<int> parent(V, -1);
+
+//     // To keep track of vertices included in MST
+//     vector<bool> inMST(V, false);
+
+//     // Insert source itself in priority queue and initialize
+//     // its key as 0.
+//     pq.push(make_pair(0, src));
+//     key[src] = 0;
+
 //     /* Looping till priority queue becomes empty */
-//     while (!pq.empty()) 
-//     { 
-//         // The first vertex in pair is the minimum key 
-//         // vertex, extract it from priority queue. 
-//         // vertex label is stored in second of pair (it 
-//         // has to be done this way to keep the vertices 
-//         // sorted key (key must be first item 
-//         // in pair) 
-//         int u = pq.top().second; 
-//         pq.pop(); 
-  
-//         inMST[u] = true; // Include vertex in MST 
-  
-//         // Traverse all adjacent of u 
-//         for (auto x : adj[u]) 
-//         { 
-//             // Get vertex label and weight of current adjacent 
-//             // of u. 
-//             int v = x.first; 
-//             int weight = x.second; 
-  
-//             // If v is not in MST and weight of (u,v) is smaller 
-//             // than current key of v 
-//             if (inMST[v] == false && key[v] > weight) 
-//             { 
-//                 // Updating key of v 
-//                 key[v] = weight; 
-//                 pq.push(make_pair(key[v], v)); 
-//                 parent[v] = u; 
-//             } 
-//         } 
-//     } 
-  
-//     // Print edges of MST using parent array 
-//     for (int i = 1; i < V; ++i) 
-//         printf("%d - %d\n", parent[i], i); 
+//     while (!pq.empty())
+//     {
+//         // The first vertex in pair is the minimum key
+//         // vertex, extract it from priority queue.
+//         // vertex label is stored in second of pair (it
+//         // has to be done this way to keep the vertices
+//         // sorted key (key must be first item
+//         // in pair)
+//         int u = pq.top().second;
+//         pq.pop();
+
+//         inMST[u] = true; // Include vertex in MST
+
+//         // Traverse all adjacent of u
+//         for (auto x : adj[u])
+//         {
+//             // Get vertex label and weight of current adjacent
+//             // of u.
+//             int v = x.first;
+//             int weight = x.second;
+
+//             // If v is not in MST and weight of (u,v) is smaller
+//             // than current key of v
+//             if (inMST[v] == false && key[v] > weight)
+//             {
+//                 // Updating key of v
+//                 key[v] = weight;
+//                 pq.push(make_pair(key[v], v));
+//                 parent[v] = u;
+//             }
+//         }
+//     }
+
+//     // Print edges of MST using parent array
+//     for (int i = 1; i < V; ++i)
+//         printf("%d - %d\n", parent[i], i);
 // }
 
 void print_mst(vector<vector<edge>> graph)
@@ -155,6 +159,8 @@ int main(){
 
     int num_cases;
     cin >> num_cases;
+
+    cout << "handling " << num_cases << " case(s)" << endl;
 
     for(int i = 0; i < num_cases; i++)
     {
